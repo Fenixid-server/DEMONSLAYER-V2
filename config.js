081bd792c7248b726bd92f87fec87b9e46281b25
Fenixid-server/ <Privateloginemails@gmail.com>
2025-02-26 18:21:44 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,79 @@
+module.exports = {
+    autoLikeStatus: true,
+    downloadMediaStatus: false,
+    sensorNumber: true,
+    sendWelcomeMessage: true,
+    
+    blackList: [],
+    whiteList: [],
+    
+    ownerNumber: '94773010580',
+    userTimezone: 'Asia/Colombo',
+    //group contact push    
+    fenixwel1: 'Your Number Has Been Auto Saved🫠',
+    fenixwel2: 'For Status Views🌹',
+    fenixwel3: 'Save and Alert me✅',
+    //status motion puah    
+    autodetect1: 'heyyy 😸',
+    autodetect2: 'ඔයගෙ නම්බරෙ ගත්තෙ ස්ටෙටස් එකකින් 🫠',
+    
+    mongoUrl: 'mongodb+srv://Hello:Hello@cluster0.aahbv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    dbName: '94', // Replace with your database name
+
+    botDetails: {
+        botName: 'FENX ID OWNER',
+        botAge: '26',
+        botLocation: 'SRILANKA',
+        fenixSaveName: '999'
+    },
+
+    relaxWishes: 'Take a deep breath... 🌿',
+
+    // Status
+    sendTranslations: [
+        "send",
+        "envoyer",
+        "enviar",
+        "invia",
+        "senden",
+        "ส่ง",
+        "gửi",
+        "отправить",
+        "إرسال",
+        "发送",
+        "wysłać",
+        "Sent",
+        "Send",
+        "one",
+        "danna",
+        "ewnna",
+        "ewpm",
+        "ewn",
+        "එවන්න",
+        "ඔනෙ",
+        "ඔන",
+        "දාන්න",
+        "දම්",
+        "එවපං",
+        "දහම්",
+        "එවපන්",
+        "දපන්",
+        "දාපන්",
+        "දාපම්",
+        "ඔනා",
+        "ඔනේ",
+        "එවහන්",
+        "One",
+        "දෙන්නකො",
+        "ewan",
+        "dapanko",
+        "dapan"
+    ],
+
+    // Emojis
+    emojis: [
+        "🌼", "😂", "🔥", "🤍", "🥰",
+        "😎", "🪻", "🎉", "👑", "🛒",
+        "🚀", "💎", "🌟", "🧘‍♀️", "🌈"
+    ] // Addeemojis including VIP, professional, and pro emojis
+};
